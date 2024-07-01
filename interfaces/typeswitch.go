2dbde69a26df07774a5c08b7b7998ae8ccec32f1
anik587 <anik587@gmail.com>
2024-07-01 15:48:54 +0600

    switch fallthrough and goto

--- interfaces/typeswitch.go
+++ interfaces/typeswitch.go
@@ -2,15 +2,46 @@ package interfaces
 
 import "fmt"
 
-func Typeswitch(a interface {}) {
+func Typeswitch(a interface{}) {
 	switch a.(type) {
-		case int: 
-        fmt.Println("Type: int, Value:", a.(int)) 
-    case string: 
-        fmt.Println("\nType: string, Value: ", a.(string)) 
-    case float64: 
-        fmt.Println("\nType: float64, Value: ", a.(float64)) 
-    default: 
-        fmt.Println("\nType not found") 
+	case int:
+		fmt.Println("Type: int, Value:", a.(int))
+	case string:
+		fmt.Println("\nType: string, Value: ", a.(string))
+	case float64:
+		fmt.Println("\nType: float64, Value: ", a.(float64))
+	default:
+		fmt.Println("\nType not found")
+	}
+}
+
+func Fallthrough() {
+	i := 2
+	a := 5
+	b := 4
+	switch i {
+	case 1:
+		fmt.Println("from 1")
+		if b == 4 {
+			fmt.Println("from 1+4")
+			goto case2
+		}
+	case2:
+		fmt.Println("case2")
+	case 2:
+		fmt.Println("before break from 2")
+		if a == 5 {
+			fmt.Println("before break")
+			break
+		}
+		fallthrough
+	case 3:
+		fmt.Println("from 3")
+	case 4:
+		fmt.Println("from 4")
+		fallthrough
+	default:
+		fmt.Println("from default")
+
 	}
 }
